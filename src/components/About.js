dfd66fc350ecd6fefe1cbe1823cccdaeccfa2157
CrimsonPug <ikram.tuan.mustapha@gmail.com>
2017-03-30 00:17:55 -0400

    edit about

--- src/components/About.js
+++ src/components/About.js
@@ -22,9 +22,9 @@ class About extends Component {
             </div>
             <div className="info-wrapper col-lg-6">
               <h1 className="about-title">About Ikram</h1>
-              <p>I am a full stack developer who enjoy develop scalable project, and create simple intuitive interfaces.</p>
-              <p>Currently based in Toronto, Canada, I recently graduated from Brainstation Web Development Immersive Program that taught the latest web technologies and best practices. I constantly work on various projects after that drives me to learn more about other tools and frameworks</p>
-              <p>In my free time, I play ultimate frisbee, devour all you can eat sushi, and looking for new ways to use technology to help non-profit cause.</p>
+              <p>I am a full stack developer who enjoy developing scalable project, and creating simple intuitive interfaces.</p>
+              <p>Currently based in Toronto, Canada, I recently graduated from Brainstation Web Development Immersive Program that taught the latest web technologies and best practices. I constantly work on various projects that drive me to learn more about other tools and frameworks</p>
+              <p>In my free time, I play ultimate frisbee, devour all you can eat sushi, and look for new ways to use technology to help non-profit cause.</p>
             </div>
           </div>
           <div className="timeline">
